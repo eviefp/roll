d6a1638221fdb686d51033623f2289360ec07826
Vladimir Ciobanu <admin@cvlad.info>
2020-05-15 01:41:30 +0300

    Backend product options: database, api.

--- src/backend/src/Roll/API.hs
+++ src/backend/src/Roll/API.hs
@@ -12,6 +12,7 @@ import qualified Roll.API.Category          as Category
 import qualified Roll.API.CategoryRoute     as CategoryRoute
 import qualified Roll.API.ConfiguratorRoute as ConfiguratorRoute
 import qualified Roll.API.Dev               as Dev
+import qualified Roll.API.ProductOption     as ProductOption
 import qualified Roll.API.ProductRoute      as ProductRoute
 import qualified Roll.API.ProductVariant    as ProductVariant
 import qualified Roll.API.Quote             as Quote
@@ -39,6 +40,10 @@ data Routes route =
           :: route
           :- "product"
           :> ToServantApi ProductVariant.Routes
+    , option
+          :: route
+          :- "option"
+          :> ToServantApi ProductOption.Routes
     , productRoute
           :: route
           :- "produs"
@@ -62,6 +67,7 @@ handler =
     , category          = genericServerT Category.handler
     , categoryRoute     = genericServerT CategoryRoute.handler
     , product           = genericServerT ProductVariant.handler
+    , option            = genericServerT ProductOption.handler
     , productRoute      = genericServerT ProductRoute.handler
     , configuratorRoute = genericServerT ConfiguratorRoute.handler
     , static            = Static.handler
