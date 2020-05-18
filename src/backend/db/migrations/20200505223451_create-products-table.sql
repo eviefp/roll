d7682022a2f3b4857b342a54386291a91fc0a7b9
Denisa Diaconescu <denisa.diaconescu@gmail.com>
2020-05-18 10:50:02 +0300

    Added product options and compatibility in tables

--- src/backend/db/migrations/20200505223451_create-products-table.sql
+++ src/backend/db/migrations/20200505223451_create-products-table.sql
@@ -17,35 +17,61 @@ create table "product"
     , constraint product_slug_unique unique ("slug")
     );
 
-insert into supplier ("name", "url", "description") values
-    ( 'Toso', 'https://www.toso.com/', 'From Japan' ),
-    ( 'Mottura', 'https://www.mottura.com/en/', 'From Italy' ),
-    ( 'Vako', 'https://www.vako.com/', 'From Netherlands' ),
+insert into supplier 
+    ("name"             , "url"                             , "description") values
+    -- 1 
+    ( 'Toso'            , 'https://www.toso.com/'           , 'From Japan' )         ,
+    -- 2
+    ( 'Mottura'         , 'https://www.mottura.com/en/'     , 'From Italy' )         ,
+    -- 3
+    ( 'Vako'            , 'https://www.vako.com/'           , 'From Netherlands' )   ,
+    -- 4
     ( 'Creation Baumann', 'https://www.creationbaumann.com/', 'From Switzerland <3' )
     ;
 
-insert into product ("slug", "cid", "sid", "name", "price", "description") values
-    ( 'nexty', 1, 1, 'Nexty', 100, 'Aceasta este descrierea sistemului Nexty.' ),
-    ( 'legato', 1, 1, 'Legato', 100, 'Aceasta este descrierea sistemului Legato.' ),
-    ( 'squares', 1, 3, 'Square S', 100, null ),
-    ( 'squarem', 1, 3, 'Square M', 100, null ),
-    ( 'rounds', 1, 3, 'Round S', 100, null ),
-    ( 'roundm', 1, 3, 'Round M', 100, null ),
-    ( 'esse', 1, 2, 'Esse', 100,'Aceasta este descrierea sistemului Esse. Acest sistem merge cu WAVE'),
-    ( 'separe', 1, 2, 'Separe', 100, 'Aceasta este descrierea sistemului Separe. Acesta merge cu WAVE'),
-    ( 'modulo205', 1, 2, 'Modulo 205', 100, null ),
-    ( 'modulo105', 1, 2, 'Modulo 105', 100, null ),
-    ( '1pliu', 3, null , '1-pliu', 100, 'Descrierea manoperei 1pliu' ),
-    ( '2pliuri', 3, null , '2-pliuri', 100, null ),
-    ( '3pliuri', 3, null , '3-pliuri', 100, null ),
-    ( 'pliu-ascuns', 3, null , 'Pliu ascuns', 100, null ),
-    ( 'fara-pliuri', 3, null , 'Banda fara pliuri', 100, null ),
-    ( 'creion', 3, null , 'Rejansa creion', 100, null ),
-    ( 'wave', 3, null , 'Rejansa WAVE', 100, null ),
-    ( 'inele', 3, null , 'Banda cu inele', 100, null ),
-    ( 'material1', 2, null , 'Material 1', 100, 'Un material deosebit de fin.' ),
-    ( 'material2', 2, null , 'Material 2', 100, 'Material lejer.' )
-    ;
+insert into product 
+    ("slug"        , "cid", "sid", "name"             , "price", "description") values
+    -- 1
+    ( 'nexty'      , 1    , 1    , 'Nexty'            , 100    , 'Aceasta este descrierea sistemului Nexty.' )                        ,
+    -- 2
+    ( 'legato'     , 1    , 1    , 'Legato'           , 100    , 'Aceasta este descrierea sistemului Legato.' )                       ,
+    -- 3
+    ( 'squares'    , 1    , 3    , 'Square S'         , 100    , null )                                                               ,
+    -- 4
+    ( 'squarem'    , 1    , 3    , 'Square M'         , 100    , null )                                                               ,
+    -- 5
+    ( 'rounds'     , 1    , 3    , 'Round S'          , 100    , null )                                                               ,
+    -- 6
+    ( 'roundm'     , 1    , 3    , 'Round M'          , 100    , null )                                                               ,
+    -- 7
+    ( 'esse'       , 1    , 2    , 'Esse'             , 100    ,'Aceasta este descrierea sistemului Esse. Acest sistem merge cu WAVE'),
+    -- 8
+    ( 'separe'     , 1    , 2    , 'Separe'           , 100    , 'Aceasta este descrierea sistemului Separe. Acesta merge cu WAVE')   ,
+    -- 9
+    ( 'modulo205'  , 1    , 2    , 'Modulo 205'       , 100    , null )                                                               ,
+    -- 10
+    ( 'modulo105'  , 1    , 2    , 'Modulo 105'       , 100    , null )                                                               ,
+    -- 11
+    ( '1pliu'      , 3    , null , '1-pliu'           , 100    , 'Descrierea manoperei 1pliu' )                                       ,
+    -- 12
+    ( '2pliuri'    , 3    , null , '2-pliuri'         , 100    , null )                                                               ,
+    -- 13
+    ( '3pliuri'    , 3    , null , '3-pliuri'         , 100    , null )                                                               ,
+    -- 14
+    ( 'pliu-ascuns', 3    , null , 'Pliu ascuns'      , 100    , null )                                                               ,
+    -- 15
+    ( 'fara-pliuri', 3    , null , 'Banda fara pliuri', 100    , null )                                                               ,
+    -- 16
+    ( 'creion'     , 3    , null , 'Rejansa creion'   , 100    , null )                                                               ,
+    -- 17
+    ( 'wave'       , 3    , null , 'Rejansa WAVE'     , 100    , null )                                                               ,
+    -- 18
+    ( 'inele'      , 3    , null , 'Banda cu inele'   , 100    , null )                                                               ,
+    -- 19
+    ( 'material1'  , 2    , null , 'Material 1'       , 100    , 'Un material deosebit de fin.' )                                     ,
+    -- 20
+    ( 'material2'  , 2    , null , 'Material 2'       , 100    , 'Material lejer.' )
+    ; 
 
 -- migrate:down
 drop table "product";
